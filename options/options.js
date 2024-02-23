879564bbbc0eb60a129ed9e26bf07fbe15269b82
Oliver Smith <oliver.smith@avanade.com>
2024-02-23 16:33:35 +0000

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "genaichampionninja-46613",
   url: "https://genaichampionninja-46613.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
