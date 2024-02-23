d8b8dc9c53f69deb39367699730c69d8b8ea3588
Crowdbotics <team@crowdbotics.com>
2024-02-23 15:37:28 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "genaichampionninja-46613",
+  url: "https://genaichampionninja-46613.botics.co",
+  api: "https://genaichampionninja-46613.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
